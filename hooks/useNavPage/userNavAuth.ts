8fd2eff900e9ead7bf34aef540326320f305ea25
zcy22606 <619730086@qq.com>
2023-07-22 13:59:25 +0800

    feat: learning dashboard perfection

--- hooks/useNavPage/userNavAuth.ts
+++ hooks/useNavPage/userNavAuth.ts
@@ -27,6 +27,7 @@ function useNavAuth(waitingUserData: boolean) {
 
     // 未登录
     if (isNoNeedUserInfo(pathname)) {
+      console.log('不需要登录');
       return;
     } else {
       router.push(LOGIN_PATHNAME);
