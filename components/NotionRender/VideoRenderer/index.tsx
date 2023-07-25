a6b64a89697358495bcd3ae433a31688c337be4a
zcy22606 <619730086@qq.com>
2023-07-25 15:38:36 +0800

    fix: update register email

--- components/NotionRender/VideoRenderer/index.tsx
+++ components/NotionRender/VideoRenderer/index.tsx
@@ -11,7 +11,7 @@ const VideoRenderer: FC<VideoRendererProps> = (props) => {
   console.log(source, type);
   return (
     <div>
-      <video controls className="fixed w-[44%]">
+      <video controls className="w-[80%]">
         {/* width="400px" */}
         {<source src={source[type][source[type].type].url} />}
       </video>
