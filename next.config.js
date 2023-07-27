2dabcc6e327e7d435f1d5ce618f1c8365b364f1c
zcy22606 <619730086@qq.com>
2023-07-27 16:10:47 +0800

    fix: button display exception

--- next.config.js
+++ next.config.js
@@ -40,7 +40,8 @@ const nextConfig = (phase) => {
         'hack-quest-s3-prod.s3.amazonaws.com',
         'hack-quest-s3-staging.s3.amazonaws.com',
         'hackquest-s3-dev.s3.ap-northeast-1.amazonaws.com',
-        'hackquest-s3-staging.s3.ap-northeast-1.amazonaws.com'
+        'hackquest-s3-staging.s3.ap-northeast-1.amazonaws.com',
+        'hackquest-s3-prod.s3.ap-northeast-1.amazonaws.com'
       ]
     }
   });
