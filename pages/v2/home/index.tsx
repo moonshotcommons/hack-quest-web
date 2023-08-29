ffa1eebf0e0bcd12ad0120c61a569402978796c9
yuanrenjie <yuan_renjie@founder.com.cn>
2023-08-29 14:09:28 +0800

    feat: add Home page v2

---
+++ pages/v2/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeTop from '@/components/v2/Home/Top';
+
+function Home() {
+  return (
+    <div className="">
+      <HomeTop />
+    </div>
+  );
+}
+
+export default Home;
