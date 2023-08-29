f745d4ca315a28eb39f3cdec1d15cada6054dc3a
zcy22606 <619730086@qq.com>
2023-08-29 12:40:02 +0800

    fix: fixed number list display

--- components/NotionRender/NumberListItemRenderer/index.tsx
+++ components/NotionRender/NumberListItemRenderer/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { Renderer } from '..';
 import TextRenderer from '../TextRenderer';
 import { CustomRenderType, NotionRenderType } from '../type';
@@ -12,9 +12,26 @@ interface NumberListItemRendererProps {
 const NumberListItemRenderer: FC<NumberListItemRendererProps> = (props) => {
   const { source, type, parent } = props;
   let children = parent?.isRoot ? parent.content : parent.children;
-  const index = children
-    ?.filter((child: any) => child.type === NotionRenderType.NUMBERED_LIST_ITEM)
-    .findIndex((child: any) => child.id === source.id);
+  // const index = children
+  //   ?.filter((child: any) => child.type === NotionRenderType.NUMBERED_LIST_ITEM)
+  //   .findIndex((child: any) => child.id === source.id);
+
+  const index = useMemo(() => {
+    const currentIndex = children.findIndex(
+      (child: any) => child.id === source.id
+    );
+
+    let firstIndex = 0;
+
+    for (let i = currentIndex; i >= 0; i--) {
+      if (children[i].type !== NotionRenderType.NUMBERED_LIST_ITEM) {
+        break;
+      }
+      firstIndex = i;
+    }
+    return currentIndex - firstIndex;
+  }, [children, source]);
+
   return (
     <div>
       <div className="flex gap-2 py-1">
