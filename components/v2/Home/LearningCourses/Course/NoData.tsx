f55f60bf5ab7b997dccac60861c61c5594f3f5e0
yuanrenjie <yuan_renjie@founder.com.cn>
2023-09-07 11:45:37 +0800

    fix:home nodata button link

--- components/v2/Home/LearningCourses/Course/NoData.tsx
+++ components/v2/Home/LearningCourses/Course/NoData.tsx
@@ -1,9 +1,11 @@
 import Button from '@/components/Common/Button';
 import { ProcessType } from '@/service/webApi/course/type';
+import { useRouter } from 'next/router';
 interface NoDataType {
   curTab: ProcessType;
 }
 const NoData: React.FC<NoDataType> = ({ curTab }) => {
+  const router = useRouter();
   return (
     <div className="flex flex-col items-center pb-[100px]">
       <p className="text-home-learning-track-no-data-color text-[32px]">
@@ -11,10 +13,16 @@ const NoData: React.FC<NoDataType> = ({ curTab }) => {
           ? 'You don’t have any on-going course'
           : 'You don’t have any completed course'}
       </p>
-      <Button className="w-90 mt-[30px] mb-[20px] h-[55px] bg-home-learning-track-no-data-button-add-bg text-home-learning-track-no-data-button-add-color">
+      <Button
+        onClick={() => router.push('learning-track')}
+        className="w-90 mt-[30px] mb-[20px] h-[55px] bg-home-learning-track-no-data-button-add-bg text-home-learning-track-no-data-button-add-color"
+      >
         Add a New Learning Track
       </Button>
-      <Button className="w-90 h-[55px] border border-home-learning-track-no-data-button-explore-border text-home-learning-track-no-data-button-explore-color">
+      <Button
+        onClick={() => router.push('electives')}
+        className="w-90 h-[55px] border border-home-learning-track-no-data-button-explore-border text-home-learning-track-no-data-button-explore-color"
+      >
         Explore Selective Courses
       </Button>
     </div>
