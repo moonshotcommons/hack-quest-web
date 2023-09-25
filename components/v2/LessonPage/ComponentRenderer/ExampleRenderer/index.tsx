b36a8442b7bb109469d8c8033efa5e9b4cb5fcf5
zcy22606 <619730086@qq.com>
2023-09-25 11:54:35 +0800

    feat: add jump ide link

--- components/v2/LessonPage/ComponentRenderer/ExampleRenderer/index.tsx
+++ components/v2/LessonPage/ComponentRenderer/ExampleRenderer/index.tsx
@@ -1,16 +1,25 @@
-import { FC, useState } from 'react';
+import { FC, createContext, use, useCallback, useState } from 'react';
 import { FiChevronDown } from 'react-icons/fi';
 import ComponentRenderer from '..';
 import { CustomComponent } from '../../type';
+import Button from '@/components/Common/Button';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 interface ExampleRendererProps {
   // children: ReactNode
   component: CustomComponent;
   parent: any;
 }
 
+export const ExampleContext = createContext({
+  updateExampleContent: (value: string) => {}
+});
+
 const ExampleRenderer: FC<ExampleRendererProps> = (props) => {
   const { component, parent } = props;
   const [expand, setExpand] = useState(true);
+  const [exampleContent, setExampleContent] = useState('');
+
   return (
     <div
       className={`rounded-[.625rem] py-[12px] px-[20px] bg-[#E6E6E6] flex w-full flex-col h-fit ${
@@ -32,18 +41,36 @@ const ExampleRenderer: FC<ExampleRendererProps> = (props) => {
         </span>
       </div>
       {expand && (
-        <div className="relative  mt-[20px] h-full pb-[10px]">
-          {component.children.map((child) => {
-            return (
-              <ComponentRenderer
-                key={child.id}
-                component={child}
-                parent={component}
-              ></ComponentRenderer>
-            );
-          })}
+        <div className="relative mt-[20px] h-full">
+          <ExampleContext.Provider
+            value={{
+              updateExampleContent: (value: string) => setExampleContent(value)
+            }}
+          >
+            {component.children.map((child) => {
+              return (
+                <ComponentRenderer
+                  key={child.id}
+                  component={child}
+                  parent={component}
+                ></ComponentRenderer>
+              );
+            })}
+          </ExampleContext.Provider>
         </div>
       )}
+      <Link
+        href={`http://localhost:8080?code=${btoa(exampleContent)}`}
+        target="_blank"
+        className="self-end"
+      >
+        <Button
+          ghost
+          className="text-[#0b0b0b] font-next-book text-[14px] leading-[125%] tracking-[0.28px] border py-[8px] px-[40px] border-black  hover:bg-white/50 transition"
+        >
+          Try It Out
+        </Button>
+      </Link>
     </div>
   );
 };
