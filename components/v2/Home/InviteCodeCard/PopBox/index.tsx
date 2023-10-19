21c8cd4e5ad81a31532d94e821f9870408dbed90
zcy22606 <77189709+zcy22606@users.noreply.github.com>
2023-10-19 16:47:43 +0800

    Feature/hq 400 show invite code on home page and support sharing to social media apps (#441)
    * feat: add invitation code card is displayed in the home
    
    * feat: add invite code card api data
    
    * feat: adding a shared template extension
    
    * feat: add button loading props
    
    * feat: add loading effect to the button
    
    * feat: update button loading style
    
    * feat: update treasures modal structure
    
    * feat: update treasure modal open interface
    
    * feat: update treasure modal open interface
    
    * feat: add treasure modal dig callback
    
    * feat: dig treasure
    
    * feat: digging the treasure chest can also be triggered when clicking next
    
    * feat: update share pop box

--- components/v2/Home/InviteCodeCard/PopBox/index.tsx
+++ components/v2/Home/InviteCodeCard/PopBox/index.tsx
@@ -4,19 +4,26 @@ import { FC, ReactNode } from 'react';
 interface PopBoxProps {
   children: ReactNode;
   className?: string;
+  showPopTriangle?: boolean;
 }
 
-const PopBox: FC<PopBoxProps> = (props) => {
+const PopBox: FC<PopBoxProps> = ({
+  children,
+  className,
+  showPopTriangle = false
+}) => {
   return (
     <div
       className={cn(
         `absolute z-[9] flex flex-col items-center -right-[24px] top-[24px]`,
-        props.className
+        className
       )}
     >
-      <div className="w-[24px] h-[24px] -rotate-[135deg] bg-white self-end -mb-[12px] mr-[28px] shadow-[rgba(0,0,0,0.05)_1.5px_1.5px_1.5px]"></div>
+      {showPopTriangle && (
+        <div className="w-[24px] h-[24px] -rotate-[135deg] bg-white self-end -mb-[12px] mr-[28px] shadow-[rgba(0,0,0,0.05)_1.5px_1.5px_1.5px]"></div>
+      )}
       <div className="p-5 flex flex-col bg-white rounded-[10px] w-fit gap-y-[15px] shadow-[0px_2px_8px_0px_rgba(99,99,99,0.2)]">
-        {props.children}
+        {children}
       </div>
     </div>
   );
