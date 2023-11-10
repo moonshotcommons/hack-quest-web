537451aadeec344e4f15f849bf0fc88bb062e7b7
Sma||Seven <517229333@qq.com>
2023-11-10 09:59:57 +0800

    Merge staging 11 10 (#589)
    * fix: fixed metamask unsupported (#573)
    
    * Fix bug 11 7 (#575)
    
    * fix bug
    
    * fix bug
    
    * fix:nav
    
    * fix: fixed metamask unsupported (#573)
    
    * Fix bug 11 7 (#575)
    
    * fix bug
    
    * fix bug
    
    * fix:nav
    
    * Fix nav (#579)
    
    * fix:nav userDropCard border
    
    * fix css
    
    * fix: fixed project details video not updated (#581)
    
    * Fix nav (#579)
    
    * fix:nav userDropCard border
    
    * fix css
    
    * fix: fixed project details video not updated (#581)
    
    * fixImg (#584)
    
    * fixImg (#584)
    
    ---------
    
    Co-authored-by: zcy22606 <77189709+zcy22606@users.noreply.github.com>
    Co-authored-by: Edward Zhang <67910881+edward-zzz@users.noreply.github.com>

--- components/v2/Layout/Navbar/index.tsx
+++ components/v2/Layout/Navbar/index.tsx
@@ -51,10 +51,6 @@ const NavBar: React.FC<NavBarProps> = (NavBarProps) => {
       changeShowSecondNav?.(false);
       setSecondNavData([]);
       setCurNavId('');
-    } else {
-      changeShowSecondNav?.(false);
-      setSecondNavData([]);
-      setCurNavId('');
     }
   }, [pathname, navList]);
 
