eb9d0317ba2c1d57abafe5b2f0d49aace7dfde18
zcy22606 <77189709+zcy22606@users.noreply.github.com>
2023-11-10 10:36:20 +0800

    Feature/hq 486 user profile (#593)
    * fix: fixed project details video not updated
    
    * feat: add profile page

---
+++ components/v2/User/Profile/GithubActivity/index.tsx
@@ -0,0 +1,9 @@
+import { FC, ReactNode } from 'react';
+
+interface GithubActivityProps {}
+
+const GithubActivity: FC<GithubActivityProps> = (props) => {
+  return <div>GithubActivity</div>;
+};
+
+export default GithubActivity;
