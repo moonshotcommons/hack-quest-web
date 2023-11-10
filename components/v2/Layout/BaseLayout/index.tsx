a554876e96b17392a85c3048b077b605dcf5a78a
qige12138 <517229333@qq.com>
2023-11-10 19:58:07 +0800

    fix:nav

--- components/v2/Layout/BaseLayout/index.tsx
+++ components/v2/Layout/BaseLayout/index.tsx
@@ -1,6 +1,12 @@
 import User from '@/components/v2/User';
 import { Inter } from 'next/font/google';
-import React, { ReactNode, useCallback, useMemo, useState } from 'react';
+import React, {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState
+} from 'react';
 import NavBar, { NavBarProps } from '../Navbar';
 
 import Breadcrumb from '@/components/v2/Breadcrumb';
@@ -22,7 +28,9 @@ const V2Layout: React.FC<V2LayoutProps> = ({ navbarData, children }) => {
   const renderBreadcrumb = useCallback(() => {
     const full = getFull();
     const { navList } = navbarData;
-    if (full || pathname === '/' || !navList.length) return null;
+    if (full || pathname === '/') {
+      return null;
+    }
     for (let menu of navList) {
       if (menu.menu.some((v) => v.path === pathname)) {
         return null;
@@ -34,6 +42,12 @@ const V2Layout: React.FC<V2LayoutProps> = ({ navbarData, children }) => {
       </div>
     );
   }, [pathname, navbarData]);
+
+  useEffect(() => {
+    if (pathname === '/') {
+      setShowSecondNav(false);
+    }
+  }, [pathname]);
 
   return (
     <div
@@ -54,7 +68,7 @@ const V2Layout: React.FC<V2LayoutProps> = ({ navbarData, children }) => {
       <div className="h-[64px] bg-[#0b0b0b]"></div>
       <div className="m-auto">
         {/* <div className={`w-full ${showSecondNav ? 'pt-[110px]' : 'pt-[64px]'}`}> */}
-        <div className={`w-full ${showSecondNav ? 'pt-[58px]' : ''}`}>
+        <div className={`w-full ${showSecondNav ? 'pt-[48px]' : ''}`}>
           {renderBreadcrumb()}
           <main className="w-full">{children}</main>
         </div>
