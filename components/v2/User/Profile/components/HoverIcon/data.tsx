ce4e8b2b574440d8dbf5f10374029e1ec810e004
zcy22606 <77189709+zcy22606@users.noreply.github.com>
2023-11-20 21:05:10 +0800

    Feature/hq 524 user profile personal links (#627)
    * fix: fixed project details video not updated
    
    * feat: user profile personal links
    
    * fix: update modal background color
    
    * fix: fixed navbar flashing issue
    
    * feat: update profile icon style

--- components/v2/User/Profile/components/HoverIcon/data.tsx
+++ components/v2/User/Profile/components/HoverIcon/data.tsx
@@ -1,33 +1,13 @@
 import { FiRefreshCw, FiLink, FiEye, FiEyeOff } from 'react-icons/fi';
 import { CiEdit } from 'react-icons/ci';
-import { IconType, IconValue } from './type';
+import { IconType } from './type';
 import { BoxType } from '../../type';
 import { GoUnlink } from 'react-icons/go';
 
-export const iconList: IconType[] = [
-  {
-    icon: <FiRefreshCw size={20} />,
-    value: IconValue.REFRESH,
-    type: [BoxType.GITHUB_ACTIVITY]
-  },
-  {
-    icon: <GoUnlink size={20} />,
-    value: IconValue.UN_LINK,
-    type: [BoxType.GITHUB_ACTIVITY]
-  },
-  {
-    icon: <CiEdit size={26} />,
-    value: IconValue.EDIT,
-    type: [BoxType.EXPERIENCE]
-  },
-  {
-    icon: <FiEye size={20} />,
-    value: IconValue.SHOW,
-    type: []
-  },
-  {
-    icon: <FiEyeOff size={20} />,
-    value: IconValue.UN_SHOW,
-    type: []
-  }
-];
+export const hoverIcons = {
+  [IconType.EDIT]: <CiEdit size={26} />,
+  [IconType.REFRESH]: <FiRefreshCw size={20} />,
+  [IconType.UN_LINK]: <GoUnlink size={20} />,
+  [IconType.SHOW]: <FiEye size={20} />,
+  [IconType.UN_SHOW]: <FiEyeOff size={20} />
+};
--- components/v2/User/Profile/components/HoverIcon/index.tsx
+++ components/v2/User/Profile/components/HoverIcon/index.tsx
@@ -1,55 +1,87 @@
-import React, { useMemo } from 'react';
+import React, { HTMLAttributes, ReactNode, useMemo } from 'react';
 
-import { IconType, IconValue } from './type';
-import { iconList } from './data';
+import { IconType } from './type';
 import { BoxType } from '../../type';
-import Tooltip from '@/components/v2/Common/Tooltip';
+import Tooltip, { TooltipProps } from '@/components/v2/Common/Tooltip';
 import { spawn } from 'child_process';
+import { hoverIcons } from './data';
+import { cn } from '@/helper/utils';
 
 interface HoverIconProp {
-  boxType: BoxType;
-  handleClick: (type: IconValue) => void;
-  editTip?: string;
+  icon?: ReactNode;
+  type?: IconType;
+  tooltip?: string;
+  tooltipProps?: Omit<TooltipProps, 'title' | 'children'>;
+  className?: string;
 }
 
-const HoverIcon: React.FC<HoverIconProp> = ({
-  boxType,
-  handleClick,
-  editTip
-}) => {
-  const list = useMemo(() => {
-    return iconList.filter((v) => ~v.type.indexOf(boxType));
-  }, [boxType]);
+// const HoverIcon: React.FC<HoverIconProp> = ({
+//   boxType,
+//   handleClick,
+//   editTip
+// }) => {
+//   const list = useMemo(() => {
+//     return iconList.filter((v) => ~v.type.indexOf(boxType));
+//   }, [boxType]);
 
-  const renderIcon = (icon: IconType) => {
-    switch (icon.value) {
-      case IconValue.EDIT:
-        return (
-          <Tooltip key={icon.value} placement="topRight" title={editTip}>
-            <div
-              onClick={() => handleClick(icon.value)}
-              className="text-[#231F20] flex-center w-[45px] h-[45px] rounded-[50%] bg-[#f4f4f4] cursor-pointer hover:text-[#8c8c8c] hover:bg-[#DADADA]"
-            >
-              <div className="w-full h-full flex-center">{icon.icon}</div>
-            </div>
-          </Tooltip>
-        );
-      default:
-        return (
-          <div
-            key={icon.value}
-            onClick={() => handleClick(icon.value)}
-            className="text-[#231F20] flex-center w-[45px] h-[45px] rounded-[50%] bg-[#f4f4f4] cursor-pointer hover:text-[#8c8c8c] hover:bg-[#DADADA]"
-          >
-            <span>{icon.icon}</span>
-          </div>
-        );
-    }
-  };
-  return (
-    <div className="flex gap-[10px]">
-      {list.map((v: IconType, i: number) => renderIcon(v))}
+//   const renderIcon = (icon: IconType) => {
+//     switch (icon.value) {
+//       case IconValue.EDIT:
+//         return (
+//           <Tooltip key={icon.value} placement="topRight" title={editTip}>
+//             <div
+//               onClick={() => handleClick(icon.value)}
+//               className="text-[#231F20] flex-center w-[45px] h-[45px] rounded-[50%] bg-[#f4f4f4] cursor-pointer hover:text-[#8c8c8c] hover:bg-[#DADADA]"
+//             >
+//               <div className="w-full h-full flex-center">{icon.icon}</div>
+//             </div>
+//           </Tooltip>
+//         );
+//       default:
+//         return (
+//           <div
+//             key={icon.value}
+//             onClick={() => handleClick(icon.value)}
+//             className="text-[#231F20] flex-center w-[45px] h-[45px] rounded-[50%] bg-[#f4f4f4] cursor-pointer hover:text-[#8c8c8c] hover:bg-[#DADADA]"
+//           >
+//             <span>{icon.icon}</span>
+//           </div>
+//         );
+//     }
+//   };
+//   return (
+//     <div className="flex gap-[10px]">
+//       {list.map((v: IconType, i: number) => renderIcon(v))}
+//     </div>
+//   );
+// };
+
+const HoverIcon: React.FC<HoverIconProp & HTMLAttributes<HTMLDivElement>> = (
+  props
+) => {
+  const { className, tooltip, icon, type, tooltipProps, ...rest } = props;
+
+  const iconWrap = (
+    <div
+      className={cn(
+        'text-[#231F20] flex-center w-[45px] h-[45px] rounded-[50%] bg-[#f4f4f4] cursor-pointer hover:text-[#8c8c8c] hover:bg-[#DADADA]',
+        className
+      )}
+      {...rest}
+    >
+      {icon || (!!type && hoverIcons[type])}
     </div>
+  );
+
+  return (
+    <>
+      {tooltip && (
+        <Tooltip title={tooltip} {...tooltipProps}>
+          {iconWrap}
+        </Tooltip>
+      )}
+      {!tooltip && iconWrap}
+    </>
   );
 };
 
