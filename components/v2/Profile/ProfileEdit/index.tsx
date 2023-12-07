ad83d78e6407e5af5ff272b1242cb14c52f7c68c
Sma||Seven <517229333@qq.com>
2023-12-07 17:57:51 +0800

    Feature/hq 572 component for mini electives (#747)
    * feat:add profile page
    
    * feat:add profile page
    
    * fix:remove hackathon img border

--- components/v2/Profile/ProfileEdit/index.tsx
+++ components/v2/Profile/ProfileEdit/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import { FC, ReactNode, useContext } from 'react';
+import UserProfile from './UserProfile';
+import BackgroundImage from './BackgroundImage';
 
-interface ProfileEditProp {}
+interface ProfileEditProps {}
 
-const ProfileEdit: React.FC<ProfileEditProp> = () => {
-  return <div>ProfileEdit</div>;
+const ProfileEdit: FC<ProfileEditProps> = (props) => {
+  return (
+    <div className="w-full rounded-[10px] bg-white relative flex justify-between flex-col shadow-[0px_4px_8px_0px_rgba(0,0,0,0.12)]">
+      <BackgroundImage></BackgroundImage>
+      <div className="w-full flex-1">
+        <UserProfile></UserProfile>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileEdit;
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,10 +1,21 @@
 import MiniElectives from '@/components/v2/Profile/MiniElectives';
 import ProfileEdit from '@/components/v2/Profile/ProfileEdit';
-import React from 'react';
+import { BurialPoint } from '@/helper/burialPoint';
+import React, { useEffect } from 'react';
 
 interface ProfileProp {}
 
 const Profile: React.FC<ProfileProp> = () => {
+  useEffect(() => {
+    const startTime = new Date().getTime();
+    return () => {
+      const endTime = new Date().getTime();
+      const duration = endTime - startTime;
+      BurialPoint.track('profile-页面留存时间', {
+        duration
+      });
+    };
+  }, []);
   return (
     <div className="container mx-auto flex flex-col gap-[60px] py-[40px] font-next-book text-[16px] text-[#0b0b0b]">
       <ProfileEdit />
