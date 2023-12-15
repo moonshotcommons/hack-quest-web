a84ea1ac3a9c9650be1ef7c83ce549fe27de8e1e
Sma||Seven <517229333@qq.com>
2023-12-15 14:32:39 +0800

    feat: add judge ismobile (#822)

--- pages/mini/[courseId]/learn/[lessonIndex].tsx
+++ pages/mini/[courseId]/learn/[lessonIndex].tsx
@@ -2,24 +2,27 @@ import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
 import MiniCoursePage from '@/components/v2/MiniCoursePage';
+import useGetDevice from '@/hooks/useGetDevice';
 
 interface IProps {}
 
 const MiniUnit: NextPage<IProps> = (props) => {
   const router = useRouter();
   const { courseId } = router.query;
-
+  const isMobile = useGetDevice();
   if (!courseId) {
     return null;
   }
 
   return (
     <>
-      <div className="w-full h-full flex flex-col font-next-book px-[40px] bg-[#f4f4f4]">
-        {courseId && (
-          <MiniCoursePage courseId={courseId as string}></MiniCoursePage>
-        )}
-      </div>
+      {isMobile ? null : (
+        <div className="w-full h-full flex flex-col font-next-book px-[40px] bg-[#f4f4f4]">
+          {courseId && (
+            <MiniCoursePage courseId={courseId as string}></MiniCoursePage>
+          )}
+        </div>
+      )}
     </>
   );
 };
