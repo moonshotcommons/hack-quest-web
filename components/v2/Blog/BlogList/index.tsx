30b2bdaa8aa9011f962cf45846cc0dc694d7c034
Sma||Seven <517229333@qq.com>
2023-12-20 21:42:13 +0800

    feat:add ecosystem profile api (#831)

---
+++ components/v2/Blog/BlogList/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import BlogCard from '../../Business/BlogCard';
+
+interface BlogListProp {}
+
+const BlogList: React.FC<BlogListProp> = () => {
+  return (
+    <div className="mt-[80px] flex flex-wrap gap-x-[20px] gap-y-[40px]">
+      {Array.from({ length: 10 }).map((_, i) => (
+        <div className="w-[calc((100%-40px)/3)]" key={i}>
+          <BlogCard />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
--- components/v2/Profile/MiniElectives/index.tsx
+++ components/v2/Profile/MiniElectives/index.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import MiniElectiveCard from '../components/MiniElectiveCard';
+import { useRequest } from 'ahooks';
+import webApi from '@/service';
+import { EcosystemElectiveType } from '@/service/webApi/elective/type';
+import Loading from '../../Common/Loading';
 
 interface MiniElectivesProp {}
 
 const MiniElectives: React.FC<MiniElectivesProp> = () => {
+  const id = 'caddaf21-cf2c-45dd-9476-f4de119dd1c0';
+  const { data: elctiveList = [] as EcosystemElectiveType[], loading } =
+    useRequest(async () => {
+      const res = webApi.electiveApi.getProfileElective(id);
+      return res;
+    });
   return (
     <div>
       <p className="text-[28px] text-[#000] font-next-poster-Bold mb-[30px] tracking-[1.68px]">
         Minis
       </p>
       <div className="flex flex-col gap-[30px]">
-        <MiniElectiveCard />
-        <MiniElectiveCard />
+        <Loading loading={loading}>
+          {elctiveList.map((elective) => (
+            <MiniElectiveCard elective={elective} key={elective.id} />
+          ))}
+        </Loading>
       </div>
     </div>
   );
