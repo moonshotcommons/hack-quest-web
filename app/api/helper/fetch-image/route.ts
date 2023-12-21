77c053e0a3f71333227a097fd605afa1342928c2
zcy22606 <77189709+zcy22606@users.noreply.github.com>
2023-12-21 15:27:51 +0800

    fix: fixed lesson page layout error (#836)

--- app/api/helper/fetch-image/route.ts
+++ app/api/helper/fetch-image/route.ts
@@ -11,13 +11,7 @@ export async function POST(request: NextRequest, context: any) {
     const response = await axios.get<ArrayBuffer>(url, {
       responseType: 'arraybuffer'
     });
-    headerList.set('Content-Type', 'image/jpeg');
-    console.log('-------------');
-    headerList.forEach((v, k) => console.log(k, v));
-    return new NextResponse(response.data, {
-      status: 200,
-      headers: headerList
-    });
+    return new Response(response.data);
   } catch (error: any) {
     return NextResponse.json({ code: 500, msg: 'Picture request exception!' });
   }
