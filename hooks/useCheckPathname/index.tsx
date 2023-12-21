85286bb6a607941933123fe012a52143e4247a77
zcy22606 <77189709+zcy22606@users.noreply.github.com>
2023-12-21 15:52:57 +0800

    refactor: update pathname check (#838)

---
+++ hooks/useCheckPathname/index.tsx
@@ -0,0 +1,43 @@
+import { excludeLink } from '@/components/v2/Layout/Navbar/data';
+import { MenuLink } from '@/components/v2/Layout/Navbar/type';
+import { useParams, usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+
+export const useCheckPathname = () => {
+  const params = useParams();
+  const pathname = usePathname();
+
+  return useMemo(() => {
+    const isLessonPage =
+      !!params?.courseId && !!params.lessonId && pathname.includes('/learn/');
+
+    const isPreviewPage = pathname.startsWith('/preview');
+
+    const isLandingPage = pathname === '/';
+
+    const isMobileLink = pathname.startsWith('/mobile');
+
+    const isNavbarFullPage = isLessonPage || isPreviewPage;
+
+    const isExcludeBreadcrumbLink =
+      isNavbarFullPage ||
+      isMobileLink ||
+      isLandingPage ||
+      !!~excludeLink.indexOf(pathname as MenuLink);
+
+    return {
+      /** 是否是lesson page */
+      isLessonPage,
+      /** 是否是preview page */
+      isPreviewPage,
+      /** 是否是landing page */
+      isLandingPage,
+      /** 是否是移动端链接 */
+      isMobileLink,
+      /** 导航是否撑满 */
+      isNavbarFullPage,
+      /** 是否是忽略面包屑的link */
+      isExcludeBreadcrumbLink
+    };
+  }, [params, pathname]);
+};
