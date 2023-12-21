a62744952ddb2fae4692c2da89a0a1641795aba1
qige12138 <517229333@qq.com>
2023-12-21 16:02:02 +0800

    fix

--- components/Provider/MobileRedirect/index.tsx
+++ components/Provider/MobileRedirect/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { usePathname, useRouter } from 'next/navigation';
-import { FC, ReactNode, useEffect } from 'react';
-import { isMobile } from 'react-device-detect';
+import { FC, ReactNode } from 'react';
+// import { isMobile } from 'react-device-detect';
 
 interface MobileRedirectProps {
   children: ReactNode;
@@ -11,16 +11,16 @@ const MobileRedirect: FC<MobileRedirectProps> = ({ children }) => {
   const pathname = usePathname();
   const router = useRouter();
 
-  useEffect(() => {
-    if (isMobile && !pathname.startsWith('/mobile')) {
-      router.replace(`/mobile${pathname}`);
-    }
+  // useEffect(() => {
+  //   if (isMobile && !pathname.startsWith('/mobile')) {
+  //     router.replace(`/mobile${pathname}`);
+  //   }
 
-    if (!isMobile && pathname.startsWith('/mobile')) {
-      const newPathname = pathname.replace(/^\/mobile/, '');
-      router.replace(newPathname);
-    }
-  }, []);
+  //   if (!isMobile && pathname.startsWith('/mobile')) {
+  //     const newPathname = pathname.replace(/^\/mobile/, '');
+  //     router.replace(newPathname);
+  //   }
+  // }, []);
 
   return <>{children}</>;
 };
