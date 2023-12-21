70cb348f172f234ec674c4982403f96cc77635f0
Sma||Seven <517229333@qq.com>
2023-12-21 16:03:34 +0800

    Fix 12 21 (#839)
    * fix
    
    * fix

--- hooks/useRedirect/index.tsx
+++ hooks/useRedirect/index.tsx
@@ -1,16 +1,15 @@
 import { usePathname, useRouter } from 'next/navigation';
 import { useCallback } from 'react';
-import { isMobile } from 'react-device-detect';
 
 export const useRedirect = () => {
   const router = useRouter();
   const pathname = usePathname();
   const redirectToUrl = useCallback((url: string, isReplace = false) => {
-    if (isMobile) {
-      if (isReplace) router.replace(`/mobile${url}`);
-      else router.push(`/mobile${url}`);
-      return;
-    }
+    // if (isMobile) {
+    //   if (isReplace) router.replace(`/mobile${url}`);
+    //   else router.push(`/mobile${url}`);
+    //   return;
+    // }
     if (isReplace) {
       router.replace(url);
     } else {
