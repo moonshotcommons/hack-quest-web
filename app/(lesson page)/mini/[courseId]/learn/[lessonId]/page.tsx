36f23b3eda0fc6311c8f7a49d3adf1c8f7266ef9
qige12138 <517229333@qq.com>
2023-12-23 18:02:15 +0800

    feat:add mini elective mobile

--- app/(lesson page)/mini/[courseId]/learn/[lessonId]/page.tsx
+++ app/(lesson page)/mini/[courseId]/learn/[lessonId]/page.tsx
@@ -3,13 +3,11 @@ import type { NextPage } from 'next';
 
 import MiniCoursePage from '@/components/v2/MiniCoursePage';
 import { CourseType } from '@/service/webApi/course/type';
-import useGetDevice from '@/hooks/useGetDevice';
 import { useParams } from 'next/navigation';
 interface IProps {}
 
 const MiniLessonPage: NextPage<IProps> = (props) => {
   const { lessonId } = useParams();
-  const isMobile = useGetDevice();
   if (!lessonId) {
     return null;
   }
