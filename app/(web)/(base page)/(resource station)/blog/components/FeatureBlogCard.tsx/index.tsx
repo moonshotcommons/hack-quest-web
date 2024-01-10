d0c0964c927e3cc90034c7729a571dba001a962f
Sma||Seven <517229333@qq.com>
2024-01-10 13:31:19 +0800

    debug (#919)

--- app/(web)/(base page)/(resource station)/blog/components/FeatureBlogCard.tsx/index.tsx
+++ app/(web)/(base page)/(resource station)/blog/components/FeatureBlogCard.tsx/index.tsx
@@ -17,42 +17,38 @@ const FeatureBlogCard: React.FC<FeatureBlogCardProp> = ({ blog }) => {
   };
   return (
     <Link
-      className="w-full font-next-book h-[505px] overflow-hidden rounded-[10px]"
+      className="w-full font-next-book h-[505px] flex bg-[#fff] overflow-hidden rounded-[10px] shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)] cursor-pointer"
+      onClick={goBlogContent}
       href={`${MenuLink.BLOG}/${blog.id}`}
     >
-      <div
-        className="w-full h-full flex bg-[#fff] overflow-hidden rounded-[10px] shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)] cursor-pointer"
-        onClick={goBlogContent}
-      >
-        <div className="w-[900px] h-full relative  overflow-hidden">
-          <Image
-            src={blog.image}
-            fill
-            alt="blogImage"
-            className="object-cover"
-          ></Image>
-        </div>
-        <div className="flex-1 min-w-[460px] h-full flex flex-col justify-between p-[30px]">
-          <div className="flex flex-col gap-[15px] ">
-            <div className="flex gap-[10px]">
-              {blog.categories.map((v, i) => (
-                <div
-                  key={i}
-                  className="w-fit py-[3px] px-[14px] text-[#3E3E3E] text-[18px] leading-[29px] rounded-[100px] border border-[var(--neutral-medium-gray)] bg-[#DADADA]"
-                >
-                  {v}
-                </div>
-              ))}
-            </div>
-            <div className="text-[28px] leading-[45px] tracking-[0.56px] line-clamp-3">
-              {blog.title}
-            </div>
-            <div className="text-[18px] leading-[28.8px] tracking-[0.36px] line-clamp-6">
-              {blog.description}
-            </div>
+      <div className="w-[900px] h-full relative  overflow-hidden">
+        <Image
+          src={blog.image}
+          fill
+          alt="blogImage"
+          className="object-cover"
+        ></Image>
+      </div>
+      <div className="flex-1 min-w-[460px] h-full flex flex-col justify-between p-[30px]">
+        <div className="flex flex-col gap-[15px] ">
+          <div className="flex gap-[10px]">
+            {blog.categories.map((v, i) => (
+              <div
+                key={i}
+                className="w-fit py-[3px] px-[14px] text-[#3E3E3E] text-[18px] leading-[29px] rounded-[100px] border border-[var(--neutral-medium-gray)] bg-[#DADADA]"
+              >
+                {v}
+              </div>
+            ))}
           </div>
-          <BlogCardFooter blog={blog} />
+          <div className="text-[28px] leading-[45px] tracking-[0.56px] line-clamp-3">
+            {blog.title}
+          </div>
+          <div className="text-[18px] leading-[28.8px] tracking-[0.36px] line-clamp-6">
+            {blog.description}
+          </div>
         </div>
+        <BlogCardFooter blog={blog} />
       </div>
     </Link>
   );
