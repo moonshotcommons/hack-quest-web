61590c5b7909df5a9fb2f66c7b73c2251c07a8fa
Sma||Seven <517229333@qq.com>
2024-01-12 17:00:25 +0800

    Feature/learning (#932)
    * fix:css
    
    * fix:css

--- app/(web)/(base page)/(learn)/dashboard/components/Recommend/index.tsx
+++ app/(web)/(base page)/(learn)/dashboard/components/Recommend/index.tsx
@@ -17,7 +17,11 @@ const Recommend: React.FC<RecommendProp> = () => {
       <div className="mt-[16px] flex flex-wrap gap-[24px]">
         {list.map((learningTrack) => (
           <div key={learningTrack.id} className="w-[calc((100%-24px)/2)]">
-            <LearningTrackCard learningTrack={learningTrack} from="dashboard" />
+            <LearningTrackCard
+              learningTrack={learningTrack}
+              from="dashboard"
+              className="p-[16px] h-[200px]"
+            />
           </div>
         ))}
       </div>
