0b03a8b18e9c1feff8dd260516d63e007295e436
Sma||Seven <517229333@qq.com>
2024-01-16 14:04:10 +0800

    feat:add ugc footer (#954)

---
+++ app/(web)/(learn page)/ugc/components/UgcFooter/index.tsx
@@ -0,0 +1,50 @@
+import Button from '@/components/Common/Button';
+import React from 'react';
+
+interface UgcFooterProp {
+  onNextClick: VoidFunction;
+}
+
+const UgcFooter: React.FC<UgcFooterProp> = ({ onNextClick }) => {
+  const unitNavList = [
+    { propgress: 0.5 },
+    { propgress: 0.5 },
+    { propgress: 0.5 },
+    { propgress: 0.5 }
+  ];
+  const curIndex = 1;
+  const handleNext = () => {
+    onNextClick();
+  };
+  return (
+    <div className="h-[68px] bg-neutral-rich-gray flex-center px-[40px] relative transition-all">
+      <div className="max-w-[calc((100%-550px))] flex gap-[2px] overflow-auto">
+        {unitNavList.map((item, i) => (
+          <div
+            key={i}
+            className="w-[70px] h-[5px] rounded-[3px] bg-neutral-medium-gray overflow-hidden"
+          >
+            {curIndex >= i ? (
+              <div
+                className="h-full rounded-[3px] bg-yellow-dark transition-all"
+                style={{
+                  width: `${curIndex === i ? item.propgress * 100 : '100'}%`
+                }}
+              ></div>
+            ) : null}
+          </div>
+        ))}
+      </div>
+      <div className="absolute h-full top-0 right-[40px] flex items-center">
+        <Button
+          className="w-[216px] h-[48px] bg-yellow-primary button-text-m text-neutral-black"
+          onClick={handleNext}
+        >
+          NEXT
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default UgcFooter;
