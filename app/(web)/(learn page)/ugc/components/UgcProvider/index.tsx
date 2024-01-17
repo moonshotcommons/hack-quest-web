02ac96819cce7516579bea97c24b29220d82f0a5
qige12138 <517229333@qq.com>
2024-01-17 16:22:04 +0800

    feat:add ugc nav bar

--- app/(web)/(learn page)/ugc/components/UgcProvider/index.tsx
+++ app/(web)/(learn page)/ugc/components/UgcProvider/index.tsx
@@ -1,13 +1,23 @@
 'use client';
-import { FC, ReactNode } from 'react';
-import { UgcContext } from '../../constants/type';
+import { FC, ReactNode, useState } from 'react';
+import { UgcContext, NavbarDataType } from '../../constants/type';
 
 interface UgcProviderProps {
   children: ReactNode;
 }
 
 const UgcProvider: FC<UgcProviderProps> = ({ children }) => {
-  return <UgcContext.Provider value={{}}>{children}</UgcContext.Provider>;
+  const [navbarData, setNavbarData] = useState<NavbarDataType[]>([]);
+  return (
+    <UgcContext.Provider
+      value={{
+        navbarData,
+        setNavbarData: (data: NavbarDataType[]) => setNavbarData(data)
+      }}
+    >
+      {children}
+    </UgcContext.Provider>
+  );
 };
 
 export default UgcProvider;
--- app/(web)/(learn page)/ugc/constants/type.ts
+++ app/(web)/(learn page)/ugc/constants/type.ts
@@ -1,4 +1,13 @@
 import { createContext } from 'react';
 
-export interface UgcContextType {}
-export const UgcContext = createContext<UgcContextType>({});
+export interface NavbarDataType {
+  label: string;
+}
+export interface UgcContextType {
+  navbarData: NavbarDataType[];
+  setNavbarData: (data: NavbarDataType[]) => void;
+}
+export const UgcContext = createContext<UgcContextType>({
+  navbarData: [],
+  setNavbarData: () => {}
+});
