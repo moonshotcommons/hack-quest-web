25abd1cd7f52ef7b2086398890358a07d6b1a4d2
qige12138 <517229333@qq.com>
2024-01-18 16:08:02 +0800

    fix:ugc nav bar

--- components/Web/Layout/LearnPage/Navbar/index.tsx
+++ components/Web/Layout/LearnPage/Navbar/index.tsx
@@ -1,17 +1,15 @@
 import DarkLogoActive from '@/public/images/logo/dark-text-Logo-active.svg';
 import Image from 'next/image';
-import React, { ReactNode } from 'react';
+import React from 'react';
 
 import { useRedirect } from '@/hooks/useRedirect';
 import { IoExitOutline } from 'react-icons/io5';
 import { MenuLink } from '../../BasePage/Navbar/type';
 import { useCourseStore } from '@/store/zustand/courseStore';
 
-export interface NavBarProps {
-  children?: ReactNode;
-}
+export interface NavBarProps {}
 
-const NavBar: React.FC<NavBarProps> = (NavBarProps) => {
+const NavBar: React.FC<NavBarProps> = () => {
   const { redirectToUrl } = useRedirect();
   const learnPageTitle = useCourseStore((state) => state.learnPageTitle);
   const logoClick = () => {
