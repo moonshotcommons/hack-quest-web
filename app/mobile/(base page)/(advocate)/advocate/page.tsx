046186a42ab46ce5aecdd1de6704f9a1d56de158
zcy22606 <619730086@qq.com>
2024-02-29 17:45:50 +0800

    fix: hide metadata

--- app/mobile/(base page)/(advocate)/advocate/page.tsx
+++ app/mobile/(base page)/(advocate)/advocate/page.tsx
@@ -1,14 +1,13 @@
 'use client';
 import { FC } from 'react';
 import { useNeedPCRedirect } from '@/hooks/useNeedPCRedirect';
-import { Metadata } from 'next';
 interface AdvocatePageProps {}
 
-export const metadata: Metadata = {
-  alternates: {
-    canonical: 'https://www.hackquest.io/advocate'
-  }
-};
+// export const metadata: Metadata = {
+//   alternates: {
+//     canonical: 'https://www.hackquest.io/advocate'
+//   }
+// };
 
 const AdvocatePage: FC<AdvocatePageProps> = (props) => {
   useNeedPCRedirect();
