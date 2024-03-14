be04dc256118d57498f1f7c8793fb30a2b8d8a87
zcy22606 <619730086@qq.com>
2024-03-14 11:49:02 +0800

    feat: unify resource page ui style

--- app/(web)/(base page)/(resource)/hackathon/components/HackathonBox/index.tsx
+++ app/(web)/(base page)/(resource)/hackathon/components/HackathonBox/index.tsx
@@ -9,7 +9,7 @@ import Tab from './Tab';
 import { useRouter } from 'next/navigation';
 import { getSearchParamsUrl } from '@/helper/utils';
 import { MenuLink } from '@/components/Web/Layout/BasePage/Navbar/type';
-import { BsArrowRight } from 'react-icons/bs';
+import { HiArrowLongRight } from 'react-icons/hi2';
 
 interface HackathonBoxProp {
   page: number;
@@ -39,11 +39,12 @@ const HackathonBox: React.FC<HackathonBoxProp> = ({ page, curTab }) => {
   const buttonNode = () => {
     return (
       <div
-        className="body-l flex w-fit cursor-pointer items-center gap-[7px] border-b-[2px] border-b-yellow-primary pt-[40px] text-neutral-black"
+        className="body-m relative flex w-fit cursor-pointer items-center gap-[7px] text-neutral-off-black"
         onClick={() => redirectToUrl(`${MenuLink.PROJECTS}`)}
       >
         <span>View hackathon projects</span>
-        <BsArrowRight size={18}></BsArrowRight>
+        <HiArrowLongRight size={16}></HiArrowLongRight>
+        <div className="absolute bottom-0 left-0 h-[3px] w-full rounded-[2px] bg-yellow-dark"></div>
       </div>
     );
   };
