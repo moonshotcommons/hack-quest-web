a2d5a12d66ca1942264ff6c983766db706ecc341
qige12138 <517229333@qq.com>
2024-03-21 11:26:24 +0800

    fix seo

--- app/(web)/(base page)/(resource)/hackathon/projects/layout.tsx
+++ app/(web)/(base page)/(resource)/hackathon/projects/layout.tsx
@@ -1,9 +1,25 @@
+import { MenuLink } from '@/components/Web/Layout/BasePage/Navbar/type';
 import { Metadata } from 'next';
 import { FC, ReactNode } from 'react';
 
-export const metadata: Metadata = {
-  title: 'All Projects - Hackathons'
-};
+interface SearchParamsType {
+  searchParams: {};
+}
+export async function generateMetadata({
+  searchParams
+}: SearchParamsType): Promise<Metadata> {
+  let query = new URLSearchParams(searchParams).toString();
+  query = query ? '?' + query : '';
+
+  const metadata: Metadata = {
+    title: 'All Projects - Hackathons',
+    alternates: {
+      canonical: `https://www.hackquest.io/${MenuLink.PROJECTS}${query}`
+    }
+  };
+
+  return metadata;
+}
 
 interface PageLayoutProps {
   children: ReactNode;
