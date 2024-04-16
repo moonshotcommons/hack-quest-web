8b42777dfc23a904bf46c63a98c99ead3979150d
zcy22606 <619730086@qq.com>
2024-04-16 09:41:02 +0800

    fix: projects canonical

---
+++ app/[lang]/(web)/(base page)/(resource)/hackathon/projects/components/index.tsx
@@ -0,0 +1,49 @@
+'use client';
+import PageDescription from '@/components/Web/Business/PageDescription';
+import ProjectsPageBox from '../../components/ProjectsBox';
+import { BurialPoint } from '@/helper/burialPoint';
+import { useEffect, useRef, useState } from 'react';
+
+function ProjectsPage() {
+  const ProjectsPageRef = useRef<HTMLDivElement | null>(null);
+
+  const [loadNum, setLoadNum] = useState(0);
+  const [apiStatus, setApiStatus] = useState('init');
+
+  const handleScroll = () => {
+    if (apiStatus !== 'init') return;
+    const clientHeight = ProjectsPageRef.current?.clientHeight || 0;
+    const scrollTop = ProjectsPageRef.current?.scrollTop || 0;
+    const scrollHeight = ProjectsPageRef.current?.scrollHeight || 0;
+    if (clientHeight + scrollTop >= scrollHeight - 5) {
+      setLoadNum((num) => num + 1);
+    }
+  };
+  useEffect(() => {
+    const startTime = new Date().getTime();
+    return () => {
+      const endTime = new Date().getTime();
+      const duration = endTime - startTime;
+      BurialPoint.track('hackathon-all-projects-页面留存时间', {
+        duration
+      });
+    };
+  }, []);
+  return (
+    <div className="h-full overflow-auto pt-[40px]" onScroll={handleScroll} ref={ProjectsPageRef}>
+      <div className="container mx-auto">
+        <PageDescription
+          title={'Project Archive'}
+          description={
+            'Welcome to the central repository for accessing all previous projects from our various hackathons.'
+          }
+          className="pt-0"
+        />
+
+        <ProjectsPageBox loadNum={loadNum} setApiStatus={(status) => setApiStatus(status)} apiStatus={apiStatus} />
+      </div>
+    </div>
+  );
+}
+
+export default ProjectsPage;
