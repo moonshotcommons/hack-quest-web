395e63ba43a3881d3f070756949a008998fc01a1
qige12138 <517229333@qq.com>
2024-05-03 08:45:22 +0800

    rebuild

--- app/[lang]/(web)/(base page)/(learn)/ntu-course/components/Content/index.tsx
+++ app/[lang]/(web)/(base page)/(learn)/ntu-course/components/Content/index.tsx
@@ -17,6 +17,7 @@ const Content: React.FC<ContentProp> = ({ setOffsetTop }) => {
   const boxRef = useRef<HTMLDivElement>(null);
 
   const getOffsetTops = () => {
+    console.info('rebuild');
     const offsetTops = [];
     const childNodes = boxRef.current?.childNodes || [];
     for (let i = 0; i < childNodes?.length; i++) {
