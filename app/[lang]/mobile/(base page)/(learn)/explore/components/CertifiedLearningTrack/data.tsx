8c8b9b023467a71d4338237db9c10bf052c0aa0d
qige12138 <517229333@qq.com>
2024-05-23 18:35:27 +0800

    change card

---
+++ app/[lang]/mobile/(base page)/(learn)/explore/components/CertifiedLearningTrack/data.tsx
@@ -0,0 +1,30 @@
+import EcoCardCover1 from '@/public/images/learn/ecocard_cover1.png';
+import EcoCardCover2 from '@/public/images/learn/ecocard_cover2.png';
+import EcoCardCover3 from '@/public/images/learn/ecocard_cover3.png';
+import EcoCardCover4 from '@/public/images/learn/ecocard_cover4.png';
+export const list = [
+  {
+    id: '1',
+    title: 'Ethereum Developer',
+    description: 'Solana is the fastest Layer1 blockchain using Proof of History',
+    image: EcoCardCover1
+  },
+  {
+    id: '2',
+    title: 'Arbitrum Developer',
+    description: 'Solana is the fastest Layer1 blockchain using Proof of History',
+    image: EcoCardCover2
+  },
+  {
+    id: '3',
+    title: 'Solana Developer',
+    description: 'Solana is the fastest Layer1 blockchain using Proof of History',
+    image: EcoCardCover3
+  },
+  {
+    id: '4',
+    title: 'Mantle Developer',
+    description: 'Solana is the fastest Layer1 blockchain using Proof of History',
+    image: EcoCardCover4
+  }
+];
