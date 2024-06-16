8afc0c0b3ff8c919321886b9c327e2b19150d3b7
Rhys <tang.rui.yu@foxmail.com>
2024-06-17 02:12:45 +0800

    code in Quiz area can copy to clipboard now.

--- components/Web/LessonPage/PgcCustomRenderer/QuizRenderer/QuizARenderer/CodeRender/index.tsx
+++ components/Web/LessonPage/PgcCustomRenderer/QuizRenderer/QuizARenderer/CodeRender/index.tsx
@@ -1,6 +1,9 @@
 import { useQuizARendererContext } from '@/components/ComponentRenderer';
 import { WaitingRenderCodeType } from '@/hooks/courses/useParseQuiz';
 import React from 'react';
+import message from 'antd/es/message';
+import { CopyIcon } from '@/components/Common/Icon/CopyV2';
+import { BurialPoint } from '@/helper/burialPoint';
 
 interface CodeRenderType {
   waitingRenderCodes: WaitingRenderCodeType[];
@@ -12,6 +15,22 @@ const CodeRender: React.FC<CodeRenderType> = ({ waitingRenderCodes }) => {
   };
   return (
     <div className="scroll-wrap-x scroll-wrap-y relative h-full w-full overflow-auto rounded-[10px] bg-renderer-code-bg pr-[20px]">
+      <div
+        className="absolute right-[9px] top-[9px] z-[10] cursor-pointer rounded-[0.5rem] text-[#E3E3E3]"
+        onClick={async (e) => {
+          try {
+            await navigator.clipboard.writeText(
+              waitingRenderCodes.map((line) => line.content.getRichText()).join('\r\n')
+            );
+            BurialPoint.track('lesson-code复制');
+            message.success('Copy success!');
+          } catch (e) {
+            message.warning('The browser version is too low or incompatible！');
+          }
+        }}
+      >
+        <CopyIcon className="h-3 w-[10px] text-neutral-medium-gray" />
+      </div>
       <ul className="flex max-w-full flex-col gap-[5px] p-[15px]">
         {waitingRenderCodes.map((line, lineIndex) => (
           <li
