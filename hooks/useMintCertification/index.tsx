9ce1ebad77b99040da2acc237c80049d76e85bfe
zcy22606 <619730086@qq.com>
2024-06-20 22:01:28 +0800

    feat: add certificate mint action

--- hooks/useMintCertification/index.tsx
+++ hooks/useMintCertification/index.tsx
@@ -1,11 +1,10 @@
 import { ChainConfigContext } from '@/components/Provider/WagmiConfigProvider';
-import { ChainType } from '@/config/wagmi';
-import { useWriteSbtManagerSafeMint } from '@/lib/generated';
+import { useWriteCertificateNftMint } from '@/lib/generated';
 import webApi from '@/service';
+import { UserCertificateInfo } from '@/service/webApi/campaigns/type';
 import { useConnectModal } from '@rainbow-me/rainbowkit';
 import { useRequest } from 'ahooks';
 import { useContext } from 'react';
-import { parseUnits } from 'viem';
 import { useAccount, useChainId, useSwitchChain } from 'wagmi';
 
 const CONTRACT_ADDRESS = '0x8eDBf22b97f7bddC7F78AE13b348949DFa0731D3';
@@ -28,7 +27,7 @@ export const useMintCertification = (onSuccess?: (res: any) => void) => {
   const { switchChainAsync } = useSwitchChain();
   const { openConnectModal } = useConnectModal();
   const { updateInitialChainId } = useContext(ChainConfigContext);
-  const { writeContractAsync } = useWriteSbtManagerSafeMint();
+  const { writeContractAsync } = useWriteCertificateNftMint();
   const account = useAccount();
 
   const {
@@ -36,74 +35,43 @@ export const useMintCertification = (onSuccess?: (res: any) => void) => {
     runAsync: safeMintAsync,
     loading
   } = useRequest(
-    async (params: { sourceType: 'Certification'; sourceId: string; signatureId: number }) => {
+    async (certificate: UserCertificateInfo) => {
+      const { username, certificateId } = certificate;
+
       if (!account?.isConnected && openConnectModal) {
-        updateInitialChainId(ChainType.MANTLE);
+        updateInitialChainId(certificate.chainId);
         openConnectModal();
         throw new Error('Please connect your wallet first!');
       }
 
-      if (chainId !== ChainType.MANTLE) {
-        await switchChainAsync({ chainId: ChainType.MANTLE });
+      if (chainId !== certificate.chainId) {
+        await switchChainAsync({ chainId: certificate.chainId });
       }
 
-      const res = await webApi.campaignsApi.getSignature({
-        sourceId: params.sourceId,
-        sourceType: params.sourceType,
+      const res = await webApi.campaignsApi.getSignature(String(certificate.id), {
         address: account.address!
       });
 
       const data = await writeContractAsync({
-        address: CONTRACT_ADDRESS,
+        address: certificate.contract,
         account: account.address,
-        args: [account.address!, parseUnits(params.signatureId.toString(), 0), res.sig.v, res.sig.r, res.sig.s]
+        args: [
+          account.address!,
+          username,
+          String(certificate.chainId),
+          '',
+          String(certificateId),
+          '',
+          '',
+          res.signature
+        ]
       });
 
       const result = await webApi.campaignsApi.savaMintState({
-        certificationId: params.sourceId,
+        certificationId: certificate.id,
         txId: data
       });
-
       return result;
-
-      // if (metamaskConnector) {
-      //   const isAccount = await metamaskConnector.isAuthorized();
-      //   let account = null;
-      //   if (isAccount) {
-      //     account = await metamaskConnector.getAccount();
-      //   }
-      //   if (!account) {
-      //     const connectRes = await connectAsync({
-      //       connector: metamaskConnector
-      //     });
-      //     account = connectRes.account;
-      //   }
-      //   if (account) {
-      //     const chainId = await metamaskConnector.getChainId();
-      //     if (![5000].includes(chainId)) {
-      //       throw new Error(
-      //         'Please Switch to Mantle Mainnet to mint the certificate!'
-      //       );
-      //     }
-      //     const res = await webApi.campaignsApi.getSignature({
-      //       sourceId: params.sourceId,
-      //       sourceType: params.sourceType,
-      //       address: account!
-      //     });
-      //     const data = await writeAsync({
-      //       args: [account, params.signatureId, res.sig.v, res.sig.r, res.sig.s]
-      //     });
-      //     const result = await webApi.campaignsApi.savaMintState({
-      //       certificationId: params.sourceId,
-      //       txId: data.hash
-      //     });
-      //     return result;
-      //   } else {
-      //     throw new Error('Failed to link MetaMask');
-      //   }
-      // } else {
-      //   throw new Error('No metaMask connector found!');
-      // }
     },
     {
       manual: true
