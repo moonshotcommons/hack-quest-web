20caf243e5d8d60c11038749f058be2dfd9a9776
zcy22606 <619730086@qq.com>
2024-06-27 00:26:33 +0800

    feat: update project edit page

--- app/[lang]/(web)/(base page)/(resource)/hackathon/projects/[projectId]/edit/components/Content/constants/index.ts
+++ app/[lang]/(web)/(base page)/(resource)/hackathon/projects/[projectId]/edit/components/Content/constants/index.ts
@@ -12,11 +12,22 @@ export const formSchema = z.object({
   isPublic: z.boolean(),
   submitType: z.string().min(0),
   efrog: z.boolean(),
+  tagline: z.string().min(0),
+  technologies: z.string().min(0),
+  solvedProblem: z.string().min(0),
+  challenges: z.string().min(0),
+  teamID: z.string().min(0),
+  roomNumber: z.string().min(0),
+  figma: z.string().min(0),
+  playstore: z.string().min(0),
+  googleDrive: z.string().min(0),
+  other: z.string().min(0),
   croak: z.boolean(),
   contractLink: z.string().url(),
   projectLink: z.string().url(),
   socialLink: z.string().url(),
   partnerTooling: z.string().min(1).max(360),
+  demo: z.string().url(),
   intro: z
     .string()
     .min(1, {
--- app/[lang]/(web)/(base page)/(resource)/hackathon/projects/[projectId]/edit/components/Content/index.tsx
+++ app/[lang]/(web)/(base page)/(resource)/hackathon/projects/[projectId]/edit/components/Content/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from 'react';
 import { HackathonType, ProjectType } from '@/service/webApi/resourceStation/type';
 import Videos from './Videos';
 
-import { sectionData } from '../../constants';
 import { OffsetTopsType } from '../../../../../constants/type';
 
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -13,7 +12,6 @@ import { useRequest } from 'ahooks';
 import { RcFile, UploadFile } from 'antd/es/upload';
 import webApi from '@/service';
 import { errorMessage } from '@/helper/ui';
-import { formSchema } from './constants';
 import Info from './Info';
 import Others from './Others';
 import Wallet from './Wallet';
@@ -24,6 +22,8 @@ import { useRedirect } from '@/hooks/router/useRedirect';
 import ConfirmModal, { ConfirmModalRef } from '@/components/Web/Business/ConfirmModal';
 import Project from './Project';
 import Links from './Links';
+import { HackathonPartner } from '@/app/[lang]/(web)/(other)/form/hackathon/[hackathonId]/submission/[projectId]/components/constants';
+import { getSectionData } from '../../constants';
 
 interface ContentProp {
   setOffsetTop: (tops: OffsetTopsType[]) => void;
@@ -45,6 +45,7 @@ const Content: React.FC<ContentProp> = ({
   isClose
 }) => {
   const boxRef = useRef<HTMLFormElement>(null);
+  const sectionData = getSectionData(hackathon.id);
   const getOffsetTops = () => {
     const offsetTops = [];
     const childNodes = boxRef.current?.childNodes || [];
@@ -69,6 +70,52 @@ const Content: React.FC<ContentProp> = ({
 
   const links = typeof project.links === 'string' ? JSON.parse(project.links as string) : project.links;
 
+  const formSchema = z.object({
+    projectLogo: z.string().url(),
+    projectName: z.string().min(1, {
+      message: 'Project Name must be at least 2 characters.'
+    }),
+    location: z.string().min(1),
+    prizeTrack: z.string().min(1),
+    track: z.string().min(1),
+    githubLink: z.string().min(0).optional(),
+    isPublic: z.boolean(),
+    submitType: z.string().min(0),
+    efrog: z.boolean(),
+    croak: z.boolean(),
+    demo: z.string().url(),
+    figma: z.string().min(0).optional(),
+    playstore: z.string().min(0).optional(),
+    googleDrive: z.string().min(0).optional(),
+    other: z.string().min(0).optional(),
+    contractLink: z.string().url(),
+    projectLink: z.string().url(),
+    socialLink: z.string().url(),
+    partnerTooling: z.string().min(1).max(360),
+    tagline: z.string().min(hackathon.id === HackathonPartner.Hack4Bengal ? 1 : 0),
+    technologies: z.string().min(hackathon.id === HackathonPartner.Hack4Bengal ? 1 : 0),
+    solvedProblem: z.string().min(hackathon.id === HackathonPartner.Hack4Bengal ? 1 : 0),
+    challenges: z.string().min(hackathon.id === HackathonPartner.Hack4Bengal ? 1 : 0),
+    teamID: z.string().min(hackathon.id === HackathonPartner.Hack4Bengal ? 1 : 0),
+    roomNumber: z.string().min(hackathon.id === HackathonPartner.Hack4Bengal ? 1 : 0),
+    intro: z
+      .string()
+      .min(1, {
+        message: 'Intro must be at least 2 characters.'
+      })
+      .max(120, {
+        message: 'The intro field cannot exceed 160 characters'
+      }),
+    detailedIntro: z
+      .string()
+      .min(1, {
+        message: 'detailedIntro must be at least 16 characters.'
+      })
+      .max(600, {
+        message: 'The detailed intro field cannot exceed 600 characters'
+      })
+  });
+
   const defaultValues: z.infer<typeof formSchema> = {
     projectLogo: project.thumbnail,
     projectName: project.name,
@@ -80,6 +127,17 @@ const Content: React.FC<ContentProp> = ({
     isPublic: project.isOpenSource,
     githubLink: project.githubLink,
     efrog: project.efrog,
+    demo: project.demo,
+    figma: links?.figma || '',
+    playstore: links?.playstore || '',
+    googleDrive: links?.googleDrive || '',
+    other: links?.other || '',
+    tagline: project.tagline,
+    technologies: project.technologies,
+    solvedProblem: project.solvedProblem,
+    challenges: project.challenges,
+    teamID: project.teamID,
+    roomNumber: project.roomNumber,
     croak: project.croak,
     submitType: project.submitType,
     contractLink: links.contractLink || '',
@@ -97,24 +155,29 @@ const Content: React.FC<ContentProp> = ({
   const otherFormDisable =
     typeof form.getValues('isPublic') !== 'boolean' ||
     (form.getValues('isPublic') === true && !(form.getValues('githubLink') || '').trim());
+
   const infoDisable =
     !form.getValues('projectName') ||
     !form.getValues('projectLogo') ||
     !form.getValues('prizeTrack') ||
     !form.getValues('intro') ||
     !form.getValues('detailedIntro') ||
-    form.getValues('track').split(',').length < 1;
+    (form.getValues('track').split(',').length < 1 && hackathon.id !== HackathonPartner.Hack4Bengal);
 
   const projectDisable =
-    typeof form.getValues('efrog') !== 'boolean' ||
-    typeof form.getValues('croak') !== 'boolean' ||
-    !form.getValues('submitType');
+    (typeof form.getValues('efrog') !== 'boolean' ||
+      typeof form.getValues('croak') !== 'boolean' ||
+      !form.getValues('submitType')) &&
+    hackathon.id !== HackathonPartner.Hack4Bengal;
 
   const linksDisable =
-    !form.getValues('contractLink') ||
-    !form.getValues('projectLink') ||
-    !form.getValues('socialLink') ||
-    !form.getValues('partnerTooling');
+    (!form.getValues('contractLink') ||
+      !form.getValues('projectLink') ||
+      !form.getValues('socialLink') ||
+      !form.getValues('partnerTooling')) &&
+    hackathon.id !== HackathonPartner.Hack4Bengal;
+
+  const demoVideoDisable = !form.getValues('demo') && hackathon.id === HackathonPartner.Hack4Bengal;
 
   const { runAsync: onSubmitRequest, loading } = useRequest(
     async () => {
@@ -130,18 +193,65 @@ const Content: React.FC<ContentProp> = ({
       }
 
       const formData = new FormData();
-      const { projectName, track, detailedIntro, intro, prizeTrack, location, isPublic, githubLink } = values;
+      const {
+        projectName,
+        track,
+        detailedIntro,
+        intro,
+        prizeTrack,
+        location,
+        isPublic,
+        githubLink,
+        tagline,
+        solvedProblem,
+        challenges,
+        technologies,
+        teamID,
+        roomNumber,
+        contractLink,
+        projectLink,
+        socialLink,
+        partnerTooling,
+        figma,
+        playstore,
+        demo,
+        googleDrive,
+        other
+      } = values;
       formData.append('name', projectName);
       formData.append('prizeTrack', prizeTrack);
 
       track.split(',').forEach((t) => {
         formData.append('tracks[]', t);
       });
-      formData.append('location', location);
+
+      {
+        hackathon.id !== HackathonPartner.Hack4Bengal && formData.append('location', location);
+      }
       formData.append('description', detailedIntro);
       formData.append('introduction', intro);
       formData.append('isOpenSource', String(isPublic));
       formData.append('githubLink', githubLink || '');
+      formData.append('tagline', tagline || '');
+      formData.append('solvedProblem', solvedProblem || '');
+      formData.append('challenges', challenges || '');
+      formData.append('technologies', technologies || '');
+      formData.append('teamID', teamID || '');
+      formData.append('roomNumber', roomNumber || '');
+      formData.append('demo', demo || '');
+
+      const links = {
+        contractLink: contractLink || '',
+        projectLink: projectLink || '',
+        socialLink: socialLink || '',
+        partnerTooling: partnerTooling || '',
+        figma: figma || '',
+        playstore: playstore || '',
+        googleDrive: googleDrive || '',
+        other: other || ''
+      };
+
+      formData.append('links', JSON.stringify(links));
 
       logo && formData.append('thumbnail', logo?.originFileObj as RcFile);
       const res = await webApi.resourceStationApi.submitProject(formData, project.id);
@@ -185,23 +295,26 @@ const Content: React.FC<ContentProp> = ({
     <>
       <div className="relative">
         <Nav
+          sectionData={sectionData}
           curAnchorIndex={curAnchorIndex}
           offsetTops={offsetTops}
           handleClickAnchor={handleClickAnchor}
           onSava={onSubmit}
           onExit={onExit}
-          submitDisable={isClose || infoDisable || otherFormDisable || projectDisable || linksDisable}
+          submitDisable={
+            isClose || infoDisable || otherFormDisable || projectDisable || linksDisable || demoVideoDisable
+          }
         />
       </div>
       <div className="flex flex-1 flex-shrink-0 flex-col gap-[60px] pb-[84px] text-neutral-off-black">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full flex-col gap-[60px]" ref={boxRef}>
-            <Info form={form} setLogo={setLogo} hackathon={hackathon} isClose={isClose} />
-            <Project form={form} isClose={isClose} />
-            <Videos project={project} isClose={isClose} />
-            <Links form={form} isClose={isClose} />
-            <Others form={form} isClose={isClose} />
-            <Wallet project={project} isClose={isClose} />
+            <Info form={form as any} setLogo={setLogo} hackathon={hackathon} isClose={isClose} />
+            {hackathon.id === HackathonPartner.Linea && <Project form={form as any} isClose={isClose} />}
+            <Videos project={project} isClose={isClose} form={form as any} />
+            {hackathon.id === HackathonPartner.Linea && <Links form={form as any} isClose={isClose} />}
+            <Others form={form as any} isClose={isClose} project={project} />
+            {hackathon.id !== HackathonPartner.Hack4Bengal && <Wallet project={project} isClose={isClose} />}
           </form>
         </Form>
         <ConfirmModal ref={exitConfirmRef} confirmText={'Save & leave'}>
