d32a151770197771abd7dffaf11bea3dc6eda2db
qige12138 <517229333@qq.com>
2024-07-02 10:48:03 +0800

    fix:youtube url

--- helper/utils.tsx
+++ helper/utils.tsx
@@ -321,3 +321,15 @@ export const toDoubleArray = <T,>(baseArray: T[], count: number) => {
   }
   return res;
 };
+
+export const getYoutubeId = (url: string) => {
+  if (!url) return '';
+  const regex = /^(?:https?:\/\/)?(?:www\.)?youtu(?:\.be|be\.com)\/(?:watch\?v=|embed\/|v\/)?([\w-]{11})(?:\S+)?$/;
+  const match = url.match(regex);
+
+  // 如果匹配成功，则返回视频 ID
+  if (match && match[1]) {
+    return match[1];
+  }
+  return '';
+};
