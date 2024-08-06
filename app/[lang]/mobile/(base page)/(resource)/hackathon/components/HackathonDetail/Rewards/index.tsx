e11dc96181ed8176696c09d71313833d4f18c9eb
smallSeven <517229333@qq.com>
2024-08-06 23:43:15 +0800

    feat:hackathon detail page add  winners

--- app/[lang]/mobile/(base page)/(resource)/hackathon/components/HackathonDetail/Rewards/index.tsx
+++ app/[lang]/mobile/(base page)/(resource)/hackathon/components/HackathonDetail/Rewards/index.tsx
@@ -4,6 +4,8 @@ import { HackathonEditContext } from '@/app/[lang]/(web)/(base page)/(resource)/
 import useDealHackathonData from '@/hooks/resource/useDealHackathonData';
 import Winners from './Winners';
 import Reward from './Reward';
+import { useQuery } from '@tanstack/react-query';
+import webApi from '@/service';
 
 interface RewardsProp {
   hackathon: HackathonType;
@@ -13,7 +15,12 @@ const Rewards: React.FC<RewardsProp> = ({ hackathon }) => {
   const { getStepIndex } = useDealHackathonData();
   const stepIndex = getStepIndex(hackathon);
   const { isEdit } = useContext(HackathonEditContext);
-  return !isEdit && stepIndex === 2 ? <Winners hackathon={hackathon} /> : <Reward hackathon={hackathon} />;
+  const { data: rewards = [] } = useQuery({
+    enabled: stepIndex === 2 && !isEdit,
+    queryKey: ['hackathon-winner', stepIndex, isEdit],
+    queryFn: () => webApi.resourceStationApi.getHackathonRewards(hackathon.id)
+  });
+  return rewards.length > 0 ? <Winners hackathon={hackathon} rewards={rewards} /> : <Reward hackathon={hackathon} />;
 };
 
 export default Rewards;
