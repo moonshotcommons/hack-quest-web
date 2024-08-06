c843e92ce3b112b982b3166911edd3d9d849fa0f
evan <jihuawu118@gmail.com>
2024-08-06 18:01:41 +0800

    wip user profile

---
+++ components/user-profile/common/use-ethers-signer.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { BrowserProvider, JsonRpcSigner } from 'ethers';
+import type { Account, Chain, Client, Transport } from 'viem';
+import { type Config, useConnectorClient } from 'wagmi';
+
+export function clientToSigner(client: Client<Transport, Chain, Account>) {
+  const { account, chain, transport } = client;
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddress: chain.contracts?.ensRegistry?.address
+  };
+  const provider = new BrowserProvider(transport, network);
+  const signer = new JsonRpcSigner(provider, account.address);
+  return signer;
+}
+
+export function useEthersSigner({ chainId }: { chainId?: number } = {}) {
+  const { data: client } = useConnectorClient<Config>({ chainId });
+  console.log('client', client);
+  return React.useMemo(() => (client ? clientToSigner(client) : undefined), [client]);
+}
