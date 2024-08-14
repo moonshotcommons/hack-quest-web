2203b6e5ff7417dbea2373194c53f6b6100f0531
evan <jihuawu118@gmail.com>
2024-08-14 13:21:21 +0800

    fix job station

--- components/job-station/components/job-description.tsx
+++ components/job-station/components/job-description.tsx
@@ -6,8 +6,8 @@ export function JobDescription({ description }: { description: Record<string, an
   const html =
     typeof description === 'string' ? description : createEditor({ content: description?.content || [] }).getHtml();
   return (
-    <p
-      className="body-m reset-editor-style text-neutral-off-black [&_h3]:my-4 [&_p]:mb-2"
+    <div
+      className="body-m reset-editor-style text-neutral-off-black [&_h3]:my-4 [&_ol]:list-inside [&_ol]:list-decimal [&_p]:mb-2 [&_ul]:list-inside [&_ul]:list-disc"
       dangerouslySetInnerHTML={{
         __html: html
       }}
