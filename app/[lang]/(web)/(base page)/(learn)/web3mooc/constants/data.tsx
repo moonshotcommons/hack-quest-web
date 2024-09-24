3bb9fe70b0cf2dda11384d189483d006adc73aa5
zvy22606 <zcy22606@qq.com>
2024-09-25 01:27:19 +0800

    feat: update ntu speaker

--- app/[lang]/(web)/(base page)/(learn)/web3mooc/constants/data.tsx
+++ app/[lang]/(web)/(base page)/(learn)/web3mooc/constants/data.tsx
@@ -15,6 +15,7 @@ import speakersAvatar15 from '@/public/images/learn/speaker_avatar15.png';
 import speakersAvatar16 from '@/public/images/learn/speaker_avatar16.png';
 import speakersAvatar17 from '@/public/images/learn/speaker_avatar17.png';
 import speakersAvatar18 from '@/public/images/learn/speaker_avatar18.png';
+import speakersAvatar19 from '@/public/images/learn/speaker_avatar19.png';
 
 import MorphLogo from '@/public/images/learn/Morph_logo.png';
 import OpenCampusLogo from '@/public/images/learn/Open_Campus_logo.png';
@@ -202,13 +203,13 @@ export const syllabusData = [
     time: '9-10:30 AM SGT, September 10th 2024 / 9-10:30 PM EST, September 9th 2024',
     description: `Join us for an in-depth exploration of the Ethereum Virtual Machine (EVM) and its evolution through parallel execution, a groundbreaking approach that promises to significantly enhance blockchain scalability. We’ll dive into the traditional EVM architecture and Ethereum’s state management before transitioning to parallel EVM execution, a hot topic that’s drawing significant attention and investment from industry leaders like Paradigm, which supports both Monad and Reddio. By leveraging Software Transactional Memory (STM) and optimistic concurrency control, parallel execution addresses scalability bottlenecks and transforms potential conflicts into performance enhancements. This session, led by Neil Han, CEO of Reddio, will provide valuable insights into how these advancements are poised to redefine the future of decentralized computing, making it a must-attend for developers, researchers, and blockchain enthusiasts looking to stay ahead in this rapidly evolving field.`,
     speaker: 'Neil Han, Founder of reddio',
-    video: '',
-    slide: '',
+    video: 'https://drive.google.com/file/d/1EICrq9V2z0JMopA3hcFDZHDBV6IRHX1P/view',
+    slide: 'https://drive.google.com/file/d/1oDSnqDqUkeXzGp3XXIFSC0447FpkGMBl/view',
     expand: false
   },
   {
     name: 'Incentive Design and Tokenomics',
-    time: '9-10:30 AM SGT, September 17th 2024 / 9-10:30 PM EST, September 16th 2024',
+    time: '9-10:30 AM SGT, September 22th 2024 / 9-10:30 PM EST, September 21th 2024',
     description: `Dive into the core concepts of incentive design and tokenomics within Web3. We'll explore how to create a system of incentives that aligns with the goals of the project and motivates stakeholders to participate in a way that benefits the overall ecosystem. Learn about different token types (utility, security), token distribution models, and the economic forces influencing token value.`,
     speaker: 'Mohamed Ezeldin, Animoca Brands',
     video: '',
@@ -413,6 +414,13 @@ export const guestSpeakersData = [
     img: speakersAvatar14,
     description:
       'Dr. Ernie Teo is a Senior Lecturer in the Nanyang Business School at the Nanyang Technological University (NTU). He received his PhD in Economics and Game Theory from UNSW Australia in 2008. He has held teaching roles at both NTU and NUS. Shifting his trajectory towards industry involvement in 2016, Dr. Ernie Teo took on the role of Research Scientist at the IBM Center for Blockchain Innovation in Singapore. During this phase, he collaborated with various corporate clients on diverse blockchain initiatives. In a remarkable step, he co-founded Dedoco in 2020 — an enterprise application company harnessing blockchain technology. Notably, this endeavour secured an impressive $7.4 million in funding, with investment from the Temasek-backed Vertex Ventures.'
+  },
+  {
+    showMore: false,
+    name: 'Julian Peh, CEO & CoFounder of KIP Protocol',
+    img: speakersAvatar19,
+    description:
+      'Julian Peh is an veteran internet entrepreneur with four successful exits and two decades of founding leading companies in fintech, media, and ad tech. Since 2016, he has been deeply involved in Web3 and AI, bringing his extensive entrepreneurial and investment experience into the intersection of blockchain and AI.'
   }
 ];
 
